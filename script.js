18bfeba9c26e07bd276ba86677bfa3f2a8879f2a
HemanthD916 <hemanthdhamodharan675@gmail.com>
2025-12-11 03:35:12 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+  a.addEventListener('click', e=>{
+    e.preventDefault();
+    const target = document.querySelector(a.getAttribute('href'));
+    if(target) target.scrollIntoView({behavior:'smooth', block:'start'});
+  });
+});
